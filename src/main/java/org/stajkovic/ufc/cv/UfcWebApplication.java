d007d63c3f079c0afd17efdb3594f305b4cd363e
Luka Stajković <luka.stajkovic@gmail.com>
2025-05-29 15:06:46 +0200

    Inicijalni commit

---
+++ src/main/java/org/stajkovic/ufc/cv/UfcWebApplication.java
@@ -0,0 +1,13 @@
+package org.stajkovic.ufc.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/stajkovic/ufc/cv/UfcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stajkovic.ufc.cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
